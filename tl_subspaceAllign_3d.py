7534a8f63660da59f9244128a5a849253ea23a63
Hardik Prajapati <hprajapa@usc.edu>
2023-03-30 13:41:13 -0700

    3dTO2d subspace allignment

--- tl_subspaceAllign_3d.py
+++ tl_subspaceAllign_3d.py
@@ -3,6 +3,7 @@ from matplotlib import pyplot as plt
 from datetime import datetime
 from sklearn.decomposition import PCA
 from sklearn import preprocessing
+from mpl_toolkits import mplot3d
 
 def inv_sigmoid(value):
     return np.log(value/(1-value))
@@ -45,9 +46,9 @@ def gaussian_mixture_model(mus,covs,n):
     samples = np.array(samples)
     return samples
 
-def rot_2d(deg,df):
+def rot_3d(deg,df):
     rad_rot=np.deg2rad(deg)
-    rot_mat=np.array([[np.cos(rad_rot),np.sin(rad_rot)],[-np.sin(rad_rot),np.cos(rad_rot)]]) #counterclockwise positive
+    rot_mat=np.array([[1,0,0],[0,np.cos(rad_rot),np.sin(rad_rot)],[0,-np.sin(rad_rot),np.cos(rad_rot)]]) #rot around x
     df_rot=np.dot(df,rot_mat)
     return df_rot
 
@@ -80,40 +81,42 @@ def plot(df_c0,df_c1,name):
 if __name__=='__main__':
     n=1000
     # source domain: GMM - class 0
-    src_mus_c0 = [np.array([-2, 2]), np.array([-2,-2])]
-    src_covs_c0 = [np.array([[4, 0.8], [0.8, 1]]), np.array([[2, -0.2], [-0.2, 1]])]
+    src_mus_c0 = [np.array([-6, 12, -6]), np.array([-6,6,-6])]
+    src_covs_c0 = [np.array([[1, 0.8, 0.2], [0.8, 1, 0.5], [0.2, 0.5, 1]]), np.array([[0.5, 0, 0], [0, 0.5, 0], [0,0,0.5]])]
     src_c0 = gaussian_mixture_model(src_mus_c0,src_covs_c0,n)
 
     # source domain: GMM - class 1
-    src_mus_c1 = [np.array([2, 2]), np.array([8, 2])]
-    src_covs_c1 = [np.array([[1, 0.8], [0.8, 2]]), np.array([[1, -0.8], [-0.8, 1]])]
+    src_mus_c1 = [np.array([-6, -12, -6]), np.array([-6,-6, -6])]
+    src_covs_c1 = [np.array([[1, 0.8, 0.2], [0.8, 1, 0.5], [0.2,0.5,1]]),np.array([[0.5, 0, 0], [0, 0.5, 0], [0, 0, 0.5]])]
     src_c1 = gaussian_mixture_model(src_mus_c1,src_covs_c1,n)
     
     
     # plot
     # plot(src_c0,src_c1,'Source')
 
-    rotation=130 #degrees +ve: counter clockwise
+    rotation=180 #degrees +ve: counter clockwise
 
     # target domain: GMM - class 1 (rotated source class 1)
     target_c1 = gaussian_mixture_model(src_mus_c1,src_covs_c1,n)
-    target_c1 = rot_2d(rotation,target_c1)
+    target_c1 = rot_3d(rotation,target_c1)
 
     # target domain: GMM - class 0 (rotated target class 0)
     target_c0 = gaussian_mixture_model(src_mus_c0,src_covs_c0,n)
-    target_c0 = rot_2d(rotation,target_c0)
+    target_c0 = rot_3d(rotation,target_c0)
     # plot
     # plot(target_c0,target_c1,'Target')
 
     #combined plot of source domain and target domain
     fig = plt.figure()
-    plt.scatter(src_c0[:, 0], src_c0[:, 1],color='r',label='src_class_0',marker='o')
-    plt.scatter(src_c1[:, 0], src_c1[:, 1],color='r',label='src_class_1',marker='x')
-    plt.scatter(target_c0[:, 0], target_c0[:, 1],color='g',label='target_class_0',marker='o')
-    plt.scatter(target_c1[:, 0], target_c1[:, 1],color='g',label='target_class_1',marker='x')
+    ax = plt.axes(projection ="3d")
+    ax.scatter3D(src_c0[:, 0], src_c0[:, 1], src_c0[:, 2], color = "r",label='src_class_0',marker='o')
+    ax.scatter3D(src_c1[:, 0], src_c1[:, 1], src_c1[:, 2], color = "r",label='src_class_1',marker='x')
+    ax.scatter3D(target_c0[:, 0], target_c0[:, 1], target_c0[:, 2], color = "g",label='target_class_0',marker='o')
+    ax.scatter3D(target_c1[:, 0], target_c1[:, 1], target_c1[:, 2], color = "g",label='target_class_1',marker='x')
     plt.title('Gaussian Mixture Model Samples: Source|Target')
-    plt.ylabel('x2')
-    plt.xlabel('x1')
+    ax.set_xlabel('x1')
+    ax.set_ylabel('x2')
+    ax.set_zlabel('x3')
     plt.legend()
     idx=datetime.now()
     dt_string = idx.strftime('%d_%m_%Y_%H_%M_%S')
@@ -159,22 +162,23 @@ if __name__=='__main__':
     #combined plot - after scaling
     #combined plot of source domain and target domain
     fig = plt.figure()
-    plt.scatter(df_src_scaled[:n, 0], df_src_scaled[:n, 1],color='r',label='src_class_0',marker='o')
-    plt.scatter(df_src_scaled[n:, 0], df_src_scaled[n:, 1],color='r',label='src_class_1',marker='x')
-    plt.scatter(df_target_scaled[:n, 0], df_target_scaled[:n, 1],color='g',label='target_class_0',marker='o')
-    plt.scatter(df_target_scaled[n:, 0], df_target_scaled[n:, 1],color='g',label='target_class_1',marker='x')
-    plt.title('Gaussian Mixture Model Samples: Source|Target - Scaled')
-    plt.ylabel('x2')
-    plt.xlabel('x1')
+    ax = plt.axes(projection ="3d")
+    ax.scatter3D(df_src_scaled[:n, 0], df_src_scaled[:n, 1], df_src_scaled[:n, 2], color = "r",label='src_class_0',marker='o')
+    ax.scatter3D(df_src_scaled[n:, 0], df_src_scaled[n:, 1], df_src_scaled[n:, 2], color = "r",label='src_class_1',marker='x')
+    ax.scatter3D(df_target_scaled[:n, 0], df_target_scaled[:n, 1], df_target_scaled[:n, 2], color = "g",label='target_class_0',marker='o')
+    ax.scatter3D(df_target_scaled[n:, 0], df_target_scaled[n:, 1], df_target_scaled[n:, 2], color = "g",label='target_class_1',marker='x')
+    plt.title('Gaussian Mixture Model Samples: Source|Target-Scaled')
+    ax.set_xlabel('x1')
+    ax.set_ylabel('x2')
+    ax.set_zlabel('x3')
     plt.legend()
-    idx=datetime.now()
     # dt_string = idx.strftime('%d_%m_%Y_%H_%M_%S')
     f_name=dt_string+'_src_target_scaled'
     plt.savefig(f_name+'.png')
     plt.show()
 
 
-    #combined plot - after scaling and pca
+    #combined plot - after scaling and pca (3d to 2d)
     #combined plot of source domain and target domain
     fig = plt.figure()
     plt.scatter(df_src_pca[:n, 0], df_src_pca[:n, 1],color='r',label='src_class_0',marker='o')
